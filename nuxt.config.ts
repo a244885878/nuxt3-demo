39fe01c5ee4218cb8e1ac201c1c1c8501b5a1a0d
李垚 <244885878@qq.com>
2025-05-23 17:12:47 +0800

    feat: 更新

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,11 @@ export default defineNuxtConfig({
   app: {
     baseURL: "/my-nuxt",
   },
+  nitro: {
+    prerender: {
+      // 允许预渲染过程中出现错误，但不会中止整个构建
+      // 当页面返回 404 或 500 时，仍会生成对应的错误页面而不是退出
+      failOnError: false,
+    },
+  },
 });
